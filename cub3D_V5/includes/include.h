bf68a76b7b9511a150013c75f3ad3824c227ce10
dclark <dclark@student.42.fr>
2021-03-18 16:51:08 +0100

    cub3D_V5_2

--- cub3D_V5/includes/include.h
+++ cub3D_V5/includes/include.h
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 16:30:51 by dclark            #+#    #+#             */
-/*   Updated: 2021/03/18 16:44:59 by dclark           ###   ########.fr       */
+/*   Updated: 2021/03/18 16:50:50 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+
+/*
+** Return the line of the element, -1 if not found
+*/
+
+/*
+** Check each elements of the .cub file, return -1 if found an error, 1 is return
+** oterwise
+*/
+
+int	check_exist(t_elem_l, *elem_l);
+
+/*
+** ---- PARSING ----
+*/
+
+/*
+** ---- UTILS ----
+*/
 
 int	get_next_line(int fd, char **line);
 
