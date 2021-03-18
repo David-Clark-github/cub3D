405a000f299530539b8d5afcacc01342f5df6add
dclark <dclark@student.42.fr>
2021-03-18 16:45:24 +0100

    cub3D_V5_1

--- cub3D_V5/includes/include.h
+++ cub3D_V5/includes/include.h
@@ -6,11 +6,21 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 16:30:51 by dclark            #+#    #+#             */
-/*   Updated: 2021/03/18 16:31:38 by dclark           ###   ########.fr       */
+/*   Updated: 2021/03/18 16:44:59 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDE_H
 # define INCLUDE_H
 
-int	get_next_line(int fd, char *line);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
