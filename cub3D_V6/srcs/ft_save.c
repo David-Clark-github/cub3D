df5c8c19f94609fe3de6a303a5aa80d95ccfff77
dclark <dclark@student.42.fr>
2021-04-14 10:57:38 +0200

    Makefile ok

--- cub3D_V6/srcs/ft_save.c
+++ cub3D_V6/srcs/ft_save.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 10:36:36 by dclark            #+#    #+#             */
-/*   Updated: 2021/04/10 10:38:19 by dclark           ###   ########.fr       */
+/*   Updated: 2021/04/14 10:13:26 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	ft_save(t_data *data)
 	int	fd;
 
 	fd = open("save.bmp", O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
+	write(fd, "BM", 2);
 	close(fd);
 	ft_error("Image sauvée dans save.bmp", 0, data);	
 }
