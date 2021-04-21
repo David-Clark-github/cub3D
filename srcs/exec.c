bd1ba01e139cce92050d8389dec7a763f5e89604
dclark <dclark@student.42.fr>
2021-04-21 10:26:56 +0200

    ca y est c'est fait

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 10:29:10 by dclark            #+#    #+#             */
-/*   Updated: 2021/04/19 14:26:23 by dclark           ###   ########.fr       */
+/*   Updated: 2021/04/21 10:21:36 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,20 @@ static int	ft_cross(void *param)
 	return (0);
 }
 
+static int	toto(void *param)
+{
+	t_data *data;
+
+	data = param;
+	mlx_put_image_to_window(data->win.mlx, data->win.win, data->img.img, 0, 0);
+	return (1);
+}
+
 void		exec(t_data *data)
 {
 	mlx_put_image_to_window(data->win.mlx, data->win.win, data->img.img, 0, 0);
 	mlx_hook(data->win.win, 2, 1L << 0, move, data);
 	mlx_hook(data->win.win, 33, 1L << 17, ft_cross, data);
+	mlx_expose_hook(data->win.win, toto, data);
 	mlx_loop(data->win.mlx);
 }
