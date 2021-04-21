f203d1dee41b8b902b8fc019ef9b834db87aa60e
dclark <dclark@student.42.fr>
2021-04-21 11:51:14 +0200

    ca y est c'est fait 2

--- srcs/pars_xpm.c
+++ srcs/pars_xpm.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 14:26:44 by dclark            #+#    #+#             */
-/*   Updated: 2021/04/13 16:23:15 by dclark           ###   ########.fr       */
+/*   Updated: 2021/04/21 10:36:17 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ void	pars_xpm(t_data *data, int t, char *line)
 {
 	int	i;
 
-	i = 0;
+	i = 2;
 	while (line[i] != '.')
+	{
+		if (!ft_isspace(line[i]))
+			ft_error("La ligne a un défaut", 1, data);
 		i++;
+	}
 	data->text[t].img = mlx_xpm_file_to_image(data->win.mlx, &line[i],
 	&data->text[t].width, &data->text[t].height);
 	if (data->text[t].img == NULL)
-		ft_error("la texture n'a pas ete charger 1", 1, data);
+		ft_error("la texture n'a pas été charger xpm_to_file", 1, data);
 	data->text[t].addr = mlx_get_data_addr(data->text[t].img,
 	&data->text[t].bpp, &data->text[t].line_l, &data->text[t].endian);
 	if (data->text[t].addr == NULL)
-		ft_error("la texture n'a pas ete charger 2", 1, data);
+		ft_error("la texture n'a pas ete charger get_data_addr", 1, data);
 }
