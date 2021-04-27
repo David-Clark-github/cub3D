b4ddd279021d3d01ac366675cc0a5208614e1a69
dclark <dclark@student.42.fr>
2021-04-27 13:01:27 +0200

    bonus/xpm bonus/file_gnl

--- bonus/file_line_gnl_bonus.c
+++ bonus/file_line_gnl_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 13:20:42 by dclark            #+#    #+#             */
-/*   Updated: 2021/04/26 15:59:51 by dclark           ###   ########.fr       */
+/*   Updated: 2021/04/27 12:09:45 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,8 @@ char		**file_line_gnl(char *av, t_data *data)
 		get_next_line(fd, &dst[dst_i]);
 		dst_i++;
 	}
+	if (ft_strlen(dst[dst_i - 1]) == 0)
+		dst[dst_i - 1] = 0;
 	dst[dst_i] = 0;
 	close(fd);
 	return (dst);
--- bonus/pars_xpm_bonus.c
+++ bonus/pars_xpm_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 14:26:44 by dclark            #+#    #+#             */
-/*   Updated: 2021/04/13 16:23:15 by dclark           ###   ########.fr       */
+/*   Updated: 2021/04/27 11:14:33 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,14 @@ void	pars_xpm(t_data *data, int t, char *line)
 	i = 0;
 	while (ft_isalpha(line[i]) && line[i])
 		++i;
-	while (line[i] != '.')
-	{
-		if (!ft_isspace(line[i]))
-			ft_error("La ligne xpm a un defaut", 1, data);
-		i++;
-	}
+	while (ft_isspace(line[i]))
+		++i;
 	data->text[t].img = mlx_xpm_file_to_image(data->win.mlx, &line[i],
 	&data->text[t].width, &data->text[t].height);
 	if (data->text[t].img == NULL)
-		ft_error("la texture n'a pas ete charger 1", 1, data);
+		ft_error("la texture n'a pas été charger xpm_to_file", 1, data);
 	data->text[t].addr = mlx_get_data_addr(data->text[t].img,
 	&data->text[t].bpp, &data->text[t].line_l, &data->text[t].endian);
 	if (data->text[t].addr == NULL)
-		ft_error("la texture n'a pas ete charger 2", 1, data);
+		ft_error("la texture n'a pas ete charger get_data_addr", 1, data);
 }
