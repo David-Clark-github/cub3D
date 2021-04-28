6868e2e0692758477c474b2ec368cf25f8410ab8
dclark <dclark@student.42.fr>
2021-04-28 13:12:10 +0200

    nouvelle branch avec tous les leaks resolus avant le raycasting

--- bonus/id_west_bonus.c
+++ bonus/id_west_bonus.c
@@ -6,13 +6,13 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 14:35:50 by dclark            #+#    #+#             */
-/*   Updated: 2021/04/07 11:56:36 by dclark           ###   ########.fr       */
+/*   Updated: 2021/04/28 12:36:20 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include.h"
 
-int	id_west(char **cub)
+int	id_west(char **cub, t_data *data)
 {
 	int	i;
 	int	flag;
@@ -29,8 +29,8 @@ int	id_west(char **cub)
 		}
 	}
 	if (flag > 1)
-		ft_error("Il y plus de 1 elements de west", 1, 0);
+		ft_error("Il y plus de 1 elements de west", 1, data);
 	if (flag == 0)
-		ft_error("L'elements west n'existe pas", 1, 0);
+		ft_error("L'elements west n'existe pas", 1, data);
 	return (res);
 }
